e894c7e3e11ac09642e5bd18a88c896221b951d5
Maliq Dyrma <up962567@myport.ac.uk>
2023-11-10 04:18:14 +0000

    Adding Products

---
+++ app/(dashboard)/[storeId]/(routes)/products/page.tsx
@@ -0,0 +1,35 @@
+import { format } from "date-fns";
+import prismadb from "@/lib/prismadb";
+import { BillboardClient } from "./components/client";
+import { BillboardColumn } from "./components/columns";
+
+const Billboardspage = async ({
+    params
+}: {
+    params: {storeId: string;}
+}) => {
+    const billboards = await prismadb.billboard.findMany({
+        where: {
+            storeId: params.storeId
+        },
+        orderBy: {
+            createdAt: 'desc'
+        }
+    });
+
+    const formatedBillboards: BillboardColumn[] = billboards.map((item) => ({
+        id: item.id,
+        label: item.label,
+        createdAt: format(item.createdAt, 'MMMM do, yyyy')
+    }));
+
+    return (
+        <div className="flex-col">
+            <div className="flex-1 space-y-4 p-8 pt-8">
+                <BillboardClient data={formatedBillboards} />
+            </div>
+        </div>
+    );
+}
+
+export default Billboardspage;
