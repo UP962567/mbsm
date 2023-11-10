1fe4db37ddc1da4ee1e22cc7e161533b428538ef
Maliq Dyrma <up962567@myport.ac.uk>
2023-11-10 03:24:27 +0000

    Adding Size

---
+++ app/(dashboard)/[storeId]/(routes)/sizes/page.tsx
@@ -0,0 +1,36 @@
+import { format } from "date-fns";
+import prismadb from "@/lib/prismadb";
+import { SizeClient } from "./components/client";
+import { SizesColumn } from "./components/columns";
+
+const SizesPage = async ({
+    params
+}: {
+    params: {storeId: string;}
+}) => {
+    const sizes = await prismadb.size.findMany({
+        where: {
+            storeId: params.storeId
+        },
+        orderBy: {
+            createdAt: 'desc'
+        }
+    });
+
+    const formatedSizes: SizesColumn[] = sizes.map((item) => ({
+        id: item.id,
+        name: item.name,
+        value: item.value,
+        createdAt: format(item.createdAt, 'MMMM do, yyyy')
+    }));
+
+    return (
+        <div className="flex-col">
+            <div className="flex-1 space-y-4 p-8 pt-8">
+                <SizeClient data={formatedSizes} />
+            </div>
+        </div>
+    );
+}
+
+export default SizesPage;
