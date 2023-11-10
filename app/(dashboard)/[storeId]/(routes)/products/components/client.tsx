66a16a38c87ffa29ab2cfff676b83d7c54f609d1
Maliq Dyrma <up962567@myport.ac.uk>
2023-11-10 05:24:56 +0000

    Change search for product

--- app/(dashboard)/[storeId]/(routes)/products/components/client.tsx
+++ app/(dashboard)/[storeId]/(routes)/products/components/client.tsx
@@ -30,7 +30,7 @@ export const ProductClient: React.FC<ProductClientProps> = ({
         </Button>
       </div>
       <Separator />
-      <DataTable searchKey="label" columns={columns} data={data} />
+      <DataTable searchKey="name" columns={columns} data={data} />
       <Heading title="API" description="API Calls for products" />
       <Separator />
       <ApiList entityName="products" entityIdName="productId" />
