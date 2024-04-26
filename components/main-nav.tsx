a537c2c6e3bfd3130a6e24a3d4c3bb8ce0f9e186
Maliq Dyrma <up962567@myport.ac.uk>
2024-04-26 22:05:00 +0100

    NAv

--- components/main-nav.tsx
+++ components/main-nav.tsx
@@ -14,22 +14,26 @@ export function MainNav({
 
     const routes_store = [
         { href: `/${params.storeId}`, label: "Dasboard", active: pathname === `/${params.storeId}`, },
-        // { href: `/${params.storeId}/orders`, label: "Orders", active: pathname === `/${params.storeId}/orders`, },
+        // { href: `/${params.storeId}/calendar`, label: "Calendar", active: pathname === `/${params.storeId}/calendar`, },
+        // { href: `/${params.storeId}/addons`, label: "Addon", active: pathname === `/${params.storeId}/addons`, },
+        // { href: `/${params.storeId}/rooms`, label: "Room", active: pathname === `/${params.storeId}/rooms`, },
+        // { href: `/${params.storeId}/floors`, label: "Floor", active: pathname === `/${params.storeId}/floors`, },
+        // { href: `/${params.storeId}/bookings`, label: "Booking", active: pathname === `/${params.storeId}/bookings`, },
     ];
 
-return (
-    <nav className={cn("flex items-center space-x-4 lg:space-x-6", className)}>
-        {routes_store.map((route) => (
-            <Link
-                key={route.href}
-                href={route.href}
-                as={route.href}
-                className={
-                    cn("text-sm font-medium transition-colors hover:text-primary", route.active ? "text-black dark:text-white" : "text-muted-foreground")}
-            >
-                {route.label}
-            </Link>
-        ))}
-    </nav>
-)
+    return (
+        <nav className={cn("flex items-center space-x-4 lg:space-x-6", className)}>
+            {routes_store.map((route) => (
+                <Link
+                    key={route.href}
+                    href={route.href}
+                    as={route.href}
+                    className={
+                        cn("text-sm font-medium transition-colors hover:text-primary", route.active ? "text-black dark:text-white" : "text-muted-foreground")}
+                >
+                    {route.label}
+                </Link>
+            ))}
+        </nav>
+    )
 };
